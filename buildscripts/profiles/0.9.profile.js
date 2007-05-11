80cf4d1e13123be4dc69f0f14b645d5843cce3de
Alex Russell <slightlyoff@chromium.org>
2007-05-11 00:06:06 +0000

    adding dijit.dijit as a require()-able rollup. This is a stop-gap and the build notes as much. Needs to be revisited for Beta. Refs #2988.
    git-svn-id: http://svn.dojotoolkit.org/src/util/trunk@8546 560b804f-0ae3-0310-86f3-f6aa0a117693

--- buildscripts/profiles/0.9.profile.js
+++ buildscripts/profiles/0.9.profile.js
@@ -2,6 +2,18 @@ var dependencies = [
 	"dojo._base"
 ];
 
+dependencies.layers = [
+	{
+		name: "../dijit/dijit.js",
+		layerDependencies: [
+			"dojo.js"
+		],
+		dependencies: [
+			"dijit.dijit"
+		]
+	}
+];
+
 dependencies.prefixes = [
 	[ "dijit", "../dijit" ],
 	[ "dojox", "../dojox" ],
