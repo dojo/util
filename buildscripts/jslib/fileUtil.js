1eca16b1b71f1716a45b1f1925088fe2c3db31a4
Adam Peller <github@peller.org>
2007-10-08 03:08:19 +0000

    Eat BOM in build.  Thanks jbondc.  At long last, fixes #1660, refs #4658
    git-svn-id: http://svn.dojotoolkit.org/src/util/trunk@10749 560b804f-0ae3-0310-86f3-f6aa0a117693

--- buildscripts/jslib/fileUtil.js
+++ buildscripts/jslib/fileUtil.js
@@ -81,16 +81,29 @@ fileUtil.readFile = function(/*String*/path, /*String?*/encoding){
 	var file = new java.io.File(path);
 	var lineSeparator = fileUtil.getLineSeparator();
 	var input = new java.io.BufferedReader(new java.io.InputStreamReader(new java.io.FileInputStream(file), encoding));
-	try {
+	try{
 		var stringBuffer = new java.lang.StringBuffer();
-		var line = "";
-		while((line = input.readLine()) !== null){
+		var line = input.readLine();
+
+		// Byte Order Mark (BOM) - The Unicode Standard, version 3.0, page 324
+		// http://www.unicode.org/faq/utf_bom.html
+		
+		// Note that when we use utf-8, the BOM should appear as "EF BB BF", but it doesn't due to this bug in the JDK:
+		// http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4508058
+		if(line && line.length() && line.charAt(0) === 0xfeff){
+			// Eat the BOM, since we've already found the encoding on this file,
+			// and we plan to concatenating this buffer with others; the BOM should
+			// only appear at the top of a file.
+			line = line.substring(1);
+		}
+		while(line !== null){
 			stringBuffer.append(line);
 			stringBuffer.append(lineSeparator);
+			line = input.readLine();
 		}
 		//Make sure we return a JavaScript string and not a Java string.
 		return new String(stringBuffer.toString()); //String
-	} finally {
+	}finally{
 		input.close();
 	}
 }
