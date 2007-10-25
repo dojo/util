6da40be10c61e124deacdd2f072134a4301a228f
Alex Russell <slightlyoff@chromium.org>
2007-10-25 22:34:53 +0000

    update profile to do the right thing. Fixes #4867
    git-svn-id: http://svn.dojotoolkit.org/src/util/trunk@11116 560b804f-0ae3-0310-86f3-f6aa0a117693

--- buildscripts/profiles/cometd.profile.js
+++ buildscripts/profiles/cometd.profile.js
@@ -1,47 +1,15 @@
-dependencies ={
+dependencies = {
+	layers: [
+		{
+			name: "../dojox/cometd.js",
+			dependencies: [
+				"dojox.cometd"
+			]
+		}
+	],
 
-    layers:  [
-        {
-        name: "dojoCometd.js",
-        dependencies: [
-            "dojo",
-            "dojox.cometd",
-        ]
-        }
-    ],
-
-    prefixes: [
-        [ "dijit", "../dijit" ],
-        [ "dojox", "../dojox" ]
-    ]
-
+	prefixes: [
+		[ "dijit", "../dijit" ],
+		[ "dojox", "../dojox" ]
+	]
 };
-var dependencies = [
-	"dojo._base"
-];
-
-dependencies.layers = [
-	{
-		name: "cometd.js",
-		layerDependencies: [
-			"dojo.js"
-		],
-		dependencies: [
-			"dojox.io.cometd"
-		]
-	},
-	{
-		name: "../dijit/dijit.js",
-		layerDependencies: [
-			"dojo.js"
-		],
-		dependencies: [
-			"dijit.dijit"
-		]
-	}
-];
-
-dependencies.prefixes = [
-	[ "dijit", "../dijit" ],
-	[ "dojox", "../dojox" ],
-];
