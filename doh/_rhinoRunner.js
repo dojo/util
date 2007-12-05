1ae7386d66cb503f496c4f236fbec3b15bc2fff3
Adam Peller <github@peller.org>
2007-12-05 04:46:28 +0000

    DOH from command line returns non-zero code upon exit.  Thanks, Jordi. Fixes #5115.
    git-svn-id: http://svn.dojotoolkit.org/src/util/trunk@11707 560b804f-0ae3-0310-86f3-f6aa0a117693

--- doh/_rhinoRunner.js
+++ doh/_rhinoRunner.js
@@ -3,3 +3,15 @@ if(this["dojo"]){
 }
 
 doh.debug = print;
+
+// Override the doh._report method to make it quit with an 
+// appropriate exit code in case of test failures.
+(function(){
+	var oldReport = doh._report;
+	doh._report = function(){
+		oldReport.apply(doh, arguments);
+		if(this._failureCount > 0 || this._errorCount > 0){
+			quit(1);
+		}
+	}
+})();
