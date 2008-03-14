23de3a5d1402eea31f01ce6f7e8869f7245f10f5
Brad Neuberg <BradNeuberg@dojotoolkit.org>
2008-03-14 00:16:10 +0000

    fixes #6206 \!strict
    git-svn-id: http://svn.dojotoolkit.org/src/util/trunk@13077 560b804f-0ae3-0310-86f3-f6aa0a117693

--- buildscripts/jslib/buildUtil.js
+++ buildscripts/jslib/buildUtil.js
@@ -113,7 +113,7 @@ buildUtil.DojoBuildOptions = {
 	},
 	"scopeMap": {
 		defaultValue: "",
-		helpText: "Change the default dojo, dijit and dojox scope names to soemthing else. Useful if you want to "
+		helpText: "Change the default dojo, dijit and dojox scope names to something else. Useful if you want to "
 			+ "use Dojo as part of a JS library, but want to make a self-contained library with no external dojo/dijit/dojox "
 			+ "references. Format is a string that contains no spaces, and is similar to the djConfig.scopeMap value (note that the "
 			+ "backslashes below are required to avoid shell escaping):\n"
@@ -350,7 +350,7 @@ buildUtil.getDependencyList = function(/*Object*/dependencies, /*String or Array
 			var old_load = load;
 			load = function(uri){
 				try{
-					//Strip comments and pply conditional directives before tracing the dependencies.
+					//Strip comments and apply conditional directives before tracing the dependencies.
 					var text = fileUtil.readFile(uri);
 					var text = (kwArgs ? buildUtil.processConditionals(layerName, text, kwArgs) : text);
 					var text = buildUtil.removeComments(text);
