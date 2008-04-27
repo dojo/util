157ae0eca60b8223a80340d85ab181cc0b16e3be
Peter E Higgins <dante@dojotoolkit.org>
2008-04-27 05:45:44 +0000

    fixes #6615 - adds dojo-mini script into buildscripts/ with many warnings and notices, though is working as expected. refs #6609 in the sense I modidied the "official" copyright notice to be a generic url that will be more easily maintainable. I've added a "shorthand" copyright_mini.txt file used in minification, though basically retains all english wording from the original. Both of those notes ref #6616 - in that there is some copying going on touching actual source tree files before and after build until those options are available from the command line, to preserve the user's ability to use custom profiles and this script together.
    git-svn-id: http://svn.dojotoolkit.org/src/util/trunk@13414 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ buildscripts/build_mini.sh
@@ -0,0 +1,119 @@
+#!/bin/sh
+#
+# 	WARNING: experimental. Test with a normal build.sh, and use this after confident. 
+#
+# 	Run this just as you would build.sh, knowing we're stealing optimize and cssOptimize from you
+# 	You can use this as a template for your own custom build script. You NEED to specify a profile
+# 	and should pass a action and version value
+#
+#	eg:
+#		./build_mini.sh action=release profile=standard version=1.1.0-mini
+#
+#	After the build completes, all non-vital files will be removed. This works _great_ with
+# 	namespaced code in the vicinity of dojo.js (a sibling, or ../../namespace) .. Though in
+# 	the latter you MUST customize your profile.
+#
+#   WARNING: There are a lot of 'rm -f' commands in here, mostly checked, but know:
+#	Use at your own risk! 
+#
+#	NOTE: This is a fairly difinitive example of what is not needed (except in testing) ...
+
+echo "release: mini started"
+
+# FIXME: refs #6616 - could be able to set a global copyright file and null out build_release.txt
+mv copyright.txt _copyright.txt
+cp copyright_mini.txt copyright.txt
+mv build_notice.txt _build_notice.txt
+touch build_notice.txt
+
+# run the build, passing all params (our assumed params first)
+java -jar ../shrinksafe/custom_rhino.jar build.js optimize=shrinksafe cssImportIgnore=../dijit.css cssOptimize=comments.keepLines "$@"
+
+# did the release work?
+if [ -d ../../release ]; then
+
+	cd ../../release/
+
+	# remove dojox tests and demos - they all follow this convetion
+	for i in dojo/dojox/* 
+	do
+	  if [ -d $i ]; then
+	    rm -rf $i/tests/
+	    rm -rf $i/demos/ 
+	  fi
+	done
+	
+	# FIXME: any shell masters out there? this is just a list ...
+
+	# removed dijit tests
+	if [ -d dojo/dijit/tests ]; then
+		rm -rf dojo/dijit/tests/ 
+	fi
+	# and demos
+	if [ -d dojo/dijit/demos ]; then
+		rm -rf dojo/dijit/demos
+	fi
+	# and the benchTool
+	if [ -d dojo/dijit/bench ]; then
+		rm -rf dojo/dijit/bench/
+	fi
+	# the dojo tests
+	if [ -d dojo/dojo/tests ]; then
+		rm -rf dojo/dojo/tests/
+		rm -f dojo/dojo/tests.js
+	fi	
+	# and stray utils
+	if [ -d dojo/util ]; then 
+		rm -rf dojo/util/
+	fi
+	
+	# noir isn't worth including yet
+	if [ -d dojo/dijit/themes/noir ]; then
+		rm -rf dojo/dijit/themes/noir/
+		
+		# so the themes are there, lets assume that, piggyback on noir: FIXME later
+		find ./dojo/dijit/themes/ -name *.html -exec rm '{}' ';'
+		rm -rf dojo/dijit/themes/themeTesterImages/	
+
+	fi
+	# TODO: merge down to a single theme.css for any theme?
+
+	# remove uncompressed .js files
+	find . -name *.uncompressed.js -exec rm '{}' ';'
+	find . -name *.commented.css -exec rm '{}' ';'
+
+	# WARNING: templates have been inlined into the .js -- if you are using dynamic templates,
+	# or other build trickery, these lines might not work!
+	rm -rf dojo/dijit/templates/
+	rm -rf dojo/dijit/form/templates/
+	rm -rf dojo/dijit/layout/templates/
+	# NOTE: we're not doing this in DojoX because the resources/ folder (to me) is deemed
+	# ambigious, and should be treated on a per-project basis
+
+	# NOTE: if you aren't using anything in DojoX, uncomment this line:
+	# rm -rf dojo/dojox/
+	# OR get creative and only populate dojox/ folder with the projects you need, and leave alone.
+	# .. assume you didn't, and clean up all the README's (leaving LICENSE, mind you)
+	find ./dojo/dojox/ -name README -exec rm '{}' ';'
+	
+	# WARNING: if you care about _base existing (and not _always_ just dojo.js providing it) ...
+	if [ -d dojo/dojo/_base ]; then
+		# ... then comment out theses lines:
+		rm -rf dojo/dojo/_base/
+		rm -f dojo/dojo/_base.js
+	fi
+	# NOTE: we're not doing the above to dijit/_base/ because I secretly use dijit/_base functions
+	# when only using dojo.js (place.js and sniff.js in particular), and mini would break stuff ...
+
+	# last but not least
+	rm -f dojo/dojo/build.txt
+	
+	cd ../util/buildscripts/
+
+fi
+
+# cleanup from above, refs #6616
+mv _copyright.txt copyright.txt
+mv _build_notice.txt build_notice.txt
+
+echo "release: mini done!"
