c6492e14e69ff0f5ccc95c5b0f384abf6365d412
Tom Trenka <ttrenka@gmail.com>
2008-04-29 21:57:07 +0000

    Added profile for dtkapi, !strict.  Refs #6640.
    git-svn-id: http://svn.dojotoolkit.org/src/util/trunk@13460 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ buildscripts/profiles/dtkapi.profile.js
@@ -0,0 +1,17 @@
+dependencies = {
+	layers: [
+		{
+			name: "../dojo/dtkapi.js",
+			dependencies: [
+				"dojox.widget.Dialog",
+				"dojox.fx.easing"
+			]
+		}
+	],
+
+	prefixes: [
+		[ "dijit", "../dijit" ],
+		[ "dojox", "../dojox" ]
+	]
+};
+
