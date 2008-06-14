480e19aef377e945ee506cbfb9ac24a66e77e2f3
Peter E Higgins <dante@dojotoolkit.org>
2008-06-14 01:23:32 +0000

    refs #6963 - reverting mistake clobber of layers.profile
    git-svn-id: http://svn.dojotoolkit.org/src/util/trunk@14003 560b804f-0ae3-0310-86f3-f6aa0a117693

--- buildscripts/profiles/layers.profile.js
+++ buildscripts/profiles/layers.profile.js
@@ -1,19 +1,57 @@
+//This profile is used just to illustrate the layout of a layered build.
+//All layers have an implicit dependency on dojo.js.
+
+//Normally you should not specify a layer object for dojo.js. It is normally
+//implicitly built containing the dojo "base" functionality (dojo._base).
+//However, if you prefer the Dojo 0.4.x build behavior, you can specify a
+//"dojo.js" layer to get that behavior. It is shown below, but the normal
+//0.9 approach is to *not* specify it.
+
+//
 
 dependencies = {
 	layers: [
 		{
-			name: "../custom/layer.js",
-			resourceName: "custom.layer",
-			dependencies:[
-				"custom.layer"
+			//For 0.9 you normally do not specify a dojo.js layer.
+			//Note that you do not need to specify dojo.js as a dependency for
+			//other layers -- it is always an implicit dependency.
+			name: "dojo.js",
+			dependencies: [
+				"dojo.parser"
+			]
+		},
+		{
+			//This layer will be discarded, it is just used
+			//to specify some modules that should not be included
+			//in a later layer, but something that should not be
+			//saved as an actual layer output. The important property
+			//is the "discard" property. If set to true, then the layer
+			//will not be a saved layer in the release directory.
+			name: "string.discard",
+			resourceName: "string.discard",
+			discard: true,
+			//Path to the copyright file must be relative to
+			//the util/buildscripts directory, or an absolute path.
+			copyrightFile: "myCopyright.txt",
+			dependencies: [
+				"dojo.string"
+			]
+		},
+		{
+			name: "../dijit/dijit.js",
+			resourceName: "dijit.dijit",
+			layerDependencies: [
+			"string.discard"
+			],
+			dependencies: [
+				"dijit.dijit"
 			]
 		}
 	],
 
 	prefixes: [
 		[ "dijit", "../dijit" ],
-		[ "dojox", "../dojox" ],
-		[ "custom", "../custom" ]
+		[ "dojox", "../dojox" ]
 	]
 }
 
