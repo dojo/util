0fc0aa67fa18f5ce744eb45578b937d6d26b10a9
Peter E Higgins <dante@dojotoolkit.org>
2008-07-19 16:57:02 +0000

    refs #5049 - add a profile that includes the demos/ repo, and builds each demo layer in one shot.
    git-svn-id: http://svn.dojotoolkit.org/src/util/trunk@14519 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ buildscripts/profiles/demos-all.profile.js
@@ -0,0 +1,24 @@
+dependencies = {
+	layers: [
+		{
+			// the dojo.moj.oe demo
+			name: "../demos/mojo/src.js",
+			dependencies: [
+				"demos.mojo.src"
+			]
+		},
+		{
+			// the dojo.workers() demo
+			name: "../demos/skew/src.js",
+			dependencies: [
+				"demos.skew.src"
+			]
+		}
+	],
+
+	prefixes: [
+		[ "dijit", "../dijit" ],
+		[ "dojox", "../dojox" ],
+		[ "demos", "../demos" ]
+	]
+}
