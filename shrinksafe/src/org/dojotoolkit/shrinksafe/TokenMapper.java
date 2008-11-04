94331b1c9361c45ab7928a65eeaa305606b595a1
Adam Peller <github@peller.org>
2008-11-04 02:30:16 +0000

    Update licensing info.  Refs #7913
    git-svn-id: http://svn.dojotoolkit.org/src/util/trunk@15591 560b804f-0ae3-0310-86f3-f6aa0a117693

--- shrinksafe/src/org/dojotoolkit/shrinksafe/TokenMapper.java
+++ shrinksafe/src/org/dojotoolkit/shrinksafe/TokenMapper.java
@@ -1,4 +1,30 @@
-package org.dojotoolkit.shrinksafe;
+/*
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Rhino code, released
+ * May 6, 1999.
+ *
+ * The Initial Developer of the Original Code is
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 1997-1999
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Alex Russell
+ *   Richard Backhouse
+ */
+ 
+ package org.dojotoolkit.shrinksafe;
 
 import java.util.ArrayList;
 import java.util.Arrays;
