f036d1d4c020597f233c2c2d17cb5c55f8c5bae5
Adam Peller <github@peller.org>
2009-03-03 16:44:50 +0000

    restructure and improve existing shrinksafe test. Refs #5303
    git-svn-id: http://svn.dojotoolkit.org/src/util/trunk@16909 560b804f-0ae3-0310-86f3-f6aa0a117693

--- shrinksafe/tests/module.js
+++ shrinksafe/tests/module.js
@@ -1,16 +1,29 @@
 dojo.provide("shrinksafe.tests.module");
 
-doh.compress = function(path){
+shrinksafe.tests.module.getContents = function(path){
 	path = "../shrinksafe/tests/" + path;
-	return new String(Packages.org.dojotoolkit.shrinksafe.Compressor.compressScript(readFile(path), 0, 1)).toString();
+	return readFile(path);
+}
+shrinksafe.tests.module.compress = function(source){
+	return new String(Packages.org.dojotoolkit.shrinksafe.Compressor.compressScript(source, 0, 1)).toString();
 }
 
 try{
-	tests.register("nestedReference", 
+	tests.register("shrinksafe", 
 	[
-		function(t){
-			eval(doh.compress("5303.js"));
+		function nestedReference(t){
+			var original = shrinksafe.tests.module.getContents("5303.js");
+			var compressed = shrinksafe.tests.module.compress(original);
+			t.assertTrue(original.length > compressed.length);
+			t.assertTrue(compressed.indexOf("say_hello") == -1)
+			t.assertTrue(compressed.indexOf("callback") == -1)
+
+			eval(compressed);
+			// make sure it runs to completion
 			t.assertEqual("hello worldhello world", result);
+			// globals must not be renamed
+			t.assertEqual("function", typeof CallMe);
+			delete result;
 		}
 	]);
 }catch(e){
