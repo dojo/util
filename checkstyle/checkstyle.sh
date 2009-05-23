27aedbdaf13ea44326ba0b950decdffaddda9cc8
Nathan Toone <nathan@toonetown.com>
2009-05-23 17:47:02 +0000

    Refs #9278 - update shell script to point to the correct js file
    git-svn-id: http://svn.dojotoolkit.org/src/util/trunk@17581 560b804f-0ae3-0310-86f3-f6aa0a117693

--- checkstyle/checkstyle.sh
+++ checkstyle/checkstyle.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-java -classpath ../shrinksafe/js.jar:../shrinksafe/shrinksafe.jar org.mozilla.javascript.tools.shell.Main checkstyle.js "$@"
+java -classpath ../shrinksafe/js.jar:../shrinksafe/shrinksafe.jar org.mozilla.javascript.tools.shell.Main runCheckstyle.js "$@"
 
 # if you experience an "Out of Memory" error, you can increase it as follows:
-#java -Xms256m -Xmx256m -classpath ../shrinksafe/js.jar:../shrinksafe/shrinksafe.jar org.mozilla.javascript.tools.shell.Main  checkstyle.js "$@"
+#java -Xms256m -Xmx256m -classpath ../shrinksafe/js.jar:../shrinksafe/shrinksafe.jar org.mozilla.javascript.tools.shell.Main  runCheckstyle.js "$@"
