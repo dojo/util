1640dfcbc910bd460dea79c4d487191b6d51b3ac
Shane O'Sullivan <shaneosullivan1@gmail.com>
2009-10-07 15:18:01 +0000

    Refs #9278 Fixes issues with loading a file for editing in the Checkstyle viewer when the user has filtered by directory.
    git-svn-id: http://svn.dojotoolkit.org/src/util/trunk@20451 560b804f-0ae3-0310-86f3-f6aa0a117693

--- checkstyle/runCheckstyle.js
+++ checkstyle/runCheckstyle.js
@@ -16,7 +16,7 @@ if(arguments[0] == "help"){
 		+ "To specify a list of files to process, use the 'files' parameter, passing a list of space separated files, e.g.\n\t\t"
 		+ "checkstyle files=\"dojo/_base/Color.js dojo/back.js\"\n\t"
 		+ "To force the script to fail when a specified file has failed the check, use the 'failOnError' parameter, e.g.\n\t\t"
-		+ "checkstyle failOnError=true files=\"dojo/_base/Color.js dojo/back.js\"\n\t\t"
+		+ "checkstyle failOnError=true files=\"dojo/_base/Color.js dojo/back.js\"\n\t"
 		+ "To commit fixes made by the checkstyleReport.html tool, use\n\t\t"
 		+ "checkstyle commit");
 		
