f0cbb7f4386277fbf4672896abbb01ac2b56454d
Heng Liu <liucougar@gmail.com>
2011-08-03 19:16:24 +0000

    refs #13301: added an option to build.sh to specify whether to use node or java
    if the first argument passed to build.sh starts with "bin=", it will specify which environment the builder will use
    
    also print a help message if no argument is passed to builder.sh or --help is passed
    
    this commit does not fix the help message generated by the builder itself
    
    
    git-svn-id: http://svn.dojotoolkit.org/src/util/trunk@26001 560b804f-0ae3-0310-86f3-f6aa0a117693

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -1,7 +1,32 @@
 #!/bin/sh
 
-if which node > /dev/null 2>&1
-then
+use_node=1
+if [ "$#" > 1 -a "${1::4}" == "bin=" ]; then
+   case ${1:4} in
+   node)
+     use_node=0
+     ;;
+   java)
+     ;;
+   *)
+     echo "Invalid binary option: only node/java is supported"
+     exit 1
+   esac
+   shift
+else
+   which node > /dev/null 2>&1
+   use_node=$?
+fi
+if echo " $@ " | grep -q ' --help ' || [ "$#" == 0 ]; then
+   echo "builder wrapper USAGE: 
+  $0 [bin=node|java] BUILDER_OPTIONS
+	By default, if first argument does not start with \"bin=\", node will be used if available.
+
+Builder Help Messages:
+"
+fi
+
+if [[ $use_node == "0" ]]; then
    node ../../dojo/dojo.js load=build "$@"
 else
    java -Xms256m -Xmx256m  -cp ../shrinksafe/js.jar:../closureCompiler/compiler.jar:../shrinksafe/shrinksafe.jar org.mozilla.javascript.tools.shell.Main  ../../dojo/dojo.js baseUrl=../../dojo load=build "$@"
