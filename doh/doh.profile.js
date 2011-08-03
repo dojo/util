2a9f363583ce0ea0c145c869930986cc8affd4e1
Heng Liu <liucougar@gmail.com>
2011-08-03 07:39:54 +0000

    refs #11869: get rid of an error when building util: doh/_parseURLargs is not a module, so don't try to build it
    git-svn-id: http://svn.dojotoolkit.org/src/util/trunk@25988 560b804f-0ae3-0310-86f3-f6aa0a117693

--- doh/doh.profile.js
+++ doh/doh.profile.js
@@ -3,7 +3,8 @@
 		list = {
 			"doh/doh.profile":1,
 			"doh/package.json":1,
-			"doh/tests":1
+			"doh/tests":1,
+			"doh/_parseURLargs":1
 		},
 		copyOnly = function(mid){
 			return (mid in list);
