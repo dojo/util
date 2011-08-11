cc6e7138a3298c12274837bfbab12d4f30ea1f22
Mark Hayes <haysmark@us.ibm.com>
2011-08-11 16:08:02 +0000

    Update robot build script. Refs #12544.
    git-svn-id: http://svn.dojotoolkit.org/src/util/trunk@26177 560b804f-0ae3-0310-86f3-f6aa0a117693

--- doh/robot/compilerobot.bat
+++ doh/robot/compilerobot.bat
@@ -1,12 +1,9 @@
 setlocal
-set JDK15_HOME=C:\Program Files\IBM\Java60
+set JDK14_HOME=C:\Program Files\IBM\Java60
 del DOHRobot*.class
-del DOHRemoteRobot*.class
-"%JDK15_HOME%\bin\javac" -source 1.5 -target 1.5 -classpath ".;%JDK15_HOME%\jre\lib\plugin.jar" DOHRobot.java
+"%JDK14_HOME%\bin\javac" -source 1.4 -target 1.4 -classpath "%JDK14_HOME%\jre\lib\plugin.jar" DOHRobot.java
 del DOHRobot.jar
-"%JDK15_HOME%\bin\jar" cvf DOHRobot.jar DOHRobot*.class DOHRemoteRobot*.class
-"%JDK15_HOME%\bin\jarsigner" -keystore ./dohrobot DOHRobot.jar dojo <key
+"%JDK14_HOME%\bin\jar" cvf DOHRobot.jar DOHRobot*.class META-INF
+"%JDK14_HOME%\bin\jarsigner" -keystore ./dohrobot DOHRobot.jar dojo <key
 del DOHRobot*.class
-del DOHRemoteRobot*.class
 endlocal
-pause
