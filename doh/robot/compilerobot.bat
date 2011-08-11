208be9396a9b4bf6b25c143f6bddca678009e5b4
Mark Hayes <haysmark@us.ibm.com>
2011-08-11 00:43:29 +0000

    Work around Apple bug that causes the robot to not initialize correctly on OS X starting with FF4 and Safari 5.1 (seems to be something Java related that is statically baked into the browser rather than dynamically pulled from the system JVM). Should not affect Windows or Linux machines. Fixes #12544.
    git-svn-id: http://svn.dojotoolkit.org/src/util/trunk@26155 560b804f-0ae3-0310-86f3-f6aa0a117693

--- doh/robot/compilerobot.bat
+++ doh/robot/compilerobot.bat
@@ -1,10 +1,12 @@
 setlocal
-set JDK14_HOME=C:\j2sdk1.4.2_19
+set JDK15_HOME=C:\Program Files\IBM\Java60
 del DOHRobot*.class
-%JDK14_HOME%\bin\javac -target 1.4 -classpath %JDK14_HOME%\jre\lib\plugin.jar DOHRobot.java
-rem del DOHRobot.jar
-%JDK14_HOME%\bin\jar xvf DOHRobot.jar META-INF
-%JDK14_HOME%\bin\jar cvf DOHRobot.jar DOHRobot*.class META-INF
-rem %JDK14_HOME%\bin\jarsigner -keystore ./dohrobot DOHRobot.jar dojo <key
+del DOHRemoteRobot*.class
+"%JDK15_HOME%\bin\javac" -source 1.5 -target 1.5 -classpath ".;%JDK15_HOME%\jre\lib\plugin.jar" DOHRobot.java
+del DOHRobot.jar
+"%JDK15_HOME%\bin\jar" cvf DOHRobot.jar DOHRobot*.class DOHRemoteRobot*.class
+"%JDK15_HOME%\bin\jarsigner" -keystore ./dohrobot DOHRobot.jar dojo <key
 del DOHRobot*.class
+del DOHRemoteRobot*.class
 endlocal
+pause
