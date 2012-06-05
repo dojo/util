2b0577d9f1a624e50870a9db9002fc547abf1285
Christophe Jolif <cjolif@gmail.com>
2012-06-05 16:32:54 +0000

    refs #14344. Fixes for todoApp demo build (we need to exclude todoApp views from build for now).
    git-svn-id: http://svn.dojotoolkit.org/src/util/trunk@28725 560b804f-0ae3-0310-86f3-f6aa0a117693

--- buildscripts/profiles/demos-all.profile.js
+++ buildscripts/profiles/demos-all.profile.js
@@ -26,7 +26,7 @@ dependencies = {
 				"dijit.dijit-all"
 			]
 		},
-
+/*
 		// Here are the various demos with promotion:
 		{
 			name: "../demos/editor/layer.js",
@@ -315,10 +315,41 @@ dependencies = {
 			dependencies: [
 				"demos.calendar.src"
 			]
-		},
+		},*/
 		{
 			name: "../demos/todoApp/src.js",
 			dependencies: [
+				"dojox/mobile/_base",
+				"dojox/mobile/_compat",
+				"dojox/mobile/TabBar",
+				"dojox/mobile/RoundRect",
+				"dojox/mobile/TabBarButton",
+				"dojox/mobile/TextBox",
+				"dojox/mobile/TextArea",
+				"dojox/mobile/CheckBox",
+				"dojox/mobile/ExpandingTextArea",
+				"dojox/mobile/Button",
+				"dojox/mobile/RoundRect",
+				"dojox/mobile/Heading",
+				"dojox/mobile/ListItem",
+				"dojox/mobile/RoundRectList",
+				"dojox/mobile/RoundRectCategory",
+				"dojox/mobile/Switch",
+				"dojox/mobile/SimpleDialog",
+				"dojox/mobile/DatePicker",
+				"dojox/mobile/Opener",
+				"dojo/date/stamp",
+				"dojox/app/widgets/Container",
+				"dojo/store/Memory",
+				"dojo/data/ItemFileWriteStore",
+				"dojo/store/DataStore",
+				"dojox/app/utils/mvcModel",
+				"dojox/mvc/EditStoreRefListController",
+				"dojox/mvc/Repeat",
+				"dojox/mvc/Group",
+				"dojox/mvc/Output",
+				"dojox/mvc/at",
+				"dojox/app/main",
 				"demos.todoApp.src"
 			]
 		}
