4bb5e09abfd9272740a04a0b83652bccaf2b31a2
Rawld Gill <rgill@altoviso.com>
2012-07-17 18:23:58 +0000

    release file handles iaw node.js's various/new behaviors firing process exit and close events; fixes #15620; !strict
    git-svn-id: http://svn.dojotoolkit.org/src/util/trunk@29276 560b804f-0ae3-0310-86f3-f6aa0a117693

--- build/node/process.js
+++ build/node/process.js
@@ -1,5 +1,6 @@
 define(["../fileHandleThrottle", "../messages"], function(fht, messages){
-	var spawn = require.nodeRequire("child_process").spawn;
+	var version = Number(process.version.match(/\d+\.\d+/)[0]),
+		spawn = require.nodeRequire("child_process").spawn;
 	return {
 		cwd:process.cwd,
 		exit:function(code){
@@ -21,14 +22,27 @@ define(["../fileHandleThrottle", "../messages"], function(fht, messages){
 			fht.enqueue(function(){
 				var
 					text = "",
-					process = spawn(command, args);
-				process.on("exit", function(code){
-					fht.release();
-					if(code){
-						bc.log("execFailed", ["message", errorMessage, "output", text]);
-					}
-					callback && callback(code, text);
-				});
+					process = spawn(command, args),
+					status = 0,
+					finish = function(code){
+						// release when both exit and close events occur; see below
+						if(++status===2){
+							fht.release();
+							if(code){
+								bc.log("execFailed", ["message", errorMessage, "output", text]);
+							}
+							callback && callback(code, text);
+						}
+					};
+
+				process.on("exit", finish);
+				// for node>=0.8, close is called; for node <0.8 close appears to not be called (verified for 0.6)
+				// in 0.8+ releasing the file handle before close is called can use up file handles too fast (see #15620)
+				if(version>=0.8){
+					process.on("close", finish);
+				}else{
+					++status;
+				}
 				process.stdout.on("data", function(data){
 					text+= data;
 				});
