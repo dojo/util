0ad26e16eb6c6cc430280546922dac18be4ffe26
doughays-dojo <doughays@us.ibm.com>
2013-05-28 16:16:46 -0400

    This reverts commit 7b47ddc4691bf1df2dcb33b0a0ed23b78acd26d7.

--- doh/plugins/changeimpact/graph.php
+++
@@ -1,6 +0,0 @@
-<?php
-	// explore the graph on the server, not the client, to help slow machines
-	$files=filter_input(INPUT_POST,'files',FILTER_SANITIZE_URL);
-	$robot=filter_input(INPUT_POST,'robot',FILTER_SANITIZE_URL);
-	print(`java -jar ../../../shrinksafe/js.jar selectTests.js $files $robot 2>&1`);
-?>
