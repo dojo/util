7d601b8d38e2fd40e6afe07f81b9a83b92a645a3
mahays0 <mahays0@gmail.com>
2013-09-17 11:20:12 -0400

    Workaround robot errors in FF23+. Still need to figure out how to deal with zoom level. Refs #17442.
    (cherry picked from commit 23abc082d571cecce77d3957c405fd5399622b81)
    (cherry picked from commit b5c1bd5dfccfc0ed6d7b5b75b8be0cb35faefc38)

--- doh/robot/compilerobot.bat
+++ doh/robot/compilerobot.bat
@@ -1,9 +1,10 @@
 setlocal
-set JDK14_HOME=C:\Program Files\IBM\Java60
+set JDK14_HOME=C:\Program Files\IBM\Java70
 del DOHRobot*.class
-"%JDK14_HOME%\bin\javac" -source 1.4 -target 1.4 -classpath "%JDK14_HOME%\jre\lib\plugin.jar" DOHRobot.java
+"%JDK14_HOME%\bin\javac" -source 1.5 -target 1.5 -classpath "%JDK14_HOME%\jre\lib\plugin.jar" DOHRobot.java
 del DOHRobot.jar
-"%JDK14_HOME%\bin\jar" cvf DOHRobot.jar DOHRobot*.class META-INF
+"%JDK14_HOME%\bin\jar" cvf DOHRobot.jar DOHRobot*.class
+"%JDK14_HOME%\bin\jar" umvf manifest.txt DOHRobot.jar
 "%JDK14_HOME%\bin\jarsigner" -keystore ./dohrobot DOHRobot.jar dojo <key
 del DOHRobot*.class
 endlocal
