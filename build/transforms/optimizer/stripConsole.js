57d3fc251f3cd79425237a1366b0a8b13d8d3f23
Ed Hager <ehager@sitepen.com>
2014-05-21 13:06:06 -0700

    Update the regex for matching on console statements to avoid window.console.

--- build/transforms/optimizer/stripConsole.js
+++ build/transforms/optimizer/stripConsole.js
@@ -6,7 +6,7 @@ define(["../../buildControl"], function(bc){
 		}else if(bc.stripConsole === "all"){
 			consoleMethods += "|warn|error";
 		}
-		var stripConsoleRe = new RegExp("console\\.(" + consoleMethods + ")\\s*\\(", "g");
+		var stripConsoleRe = new RegExp("[^\\.]console\\.(" + consoleMethods + ")\\s*\\(", "g");
 		return function(text){
 			return text.replace(stripConsoleRe, "0 && $&");
 		};
