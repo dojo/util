f06a6d65474bb38183add7d4e0a9ca12b983877a
Colin Snover <github.com@zetafleet.com>
2014-06-13 10:01:00 -0700

    Updating source version to 1.11.0-pre

--- build/version.js
+++ build/version.js
@@ -1,8 +1,8 @@
 define([], function(){
 	var
-		rev = "$Rev: fe3c741 $".match(/[0-9a-f]{7,}/),
+		rev = "$Rev$".match(/[0-9a-f]{7,}/),
 		version= {
-			major: 1, minor: 10, patch: 0, flag: "",
+			major: 1, minor: 11, patch: 0, flag: "-pre",
 			revision: rev ? rev[0] : NaN,
 			toString: function(){
 				var v= version;
