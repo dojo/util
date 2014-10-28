81180dcfcb7e301b4fbdd32e2461362ec0499615
ben hockey <neonstalwart@gmail.com>
2014-10-28 13:15:03 -0500

    add support for sourcemaps with uglify2

--- build/transforms/optimizer/uglify_worker.js
+++ build/transforms/optimizer/uglify_worker.js
@@ -1,11 +1,11 @@
-function factory(uglify){
+function factory(uglify, fs){
 	if(!uglify){
 		throw new Error("Unknown host environment: only nodejs is supported by uglify optimizer.");
 	}
 	if(uglify.minify){
 		//uglify2, provide a uglify-1 compatible uglify function
 		var UglifyJS = uglify;
-		uglify = function(code, options){
+		uglify = function(code, options, dest, useSourceMaps){
 			//parse
 			var ast = UglifyJS.parse(code, options);
 			ast.figure_out_scope();
@@ -23,7 +23,23 @@ function factory(uglify){
 			compressed_ast.compute_char_frequency();
 			compressed_ast.mangle_names();
 
-			return compressed_ast.print_to_string(options.gen_options);
+			var gen_options = options.gen_options || {};
+			if (useSourceMaps) {
+				var source_map = gen_options.source_map || {};
+				source_map.file = options.filename.split("/").pop();
+				// account for the //>> built line
+				source_map.dest_line_diff = 1;
+				gen_options.source_map = UglifyJS.SourceMap(source_map);
+			}
+
+			var output = compressed_ast.print_to_string(gen_options);
+
+			if (useSourceMaps) {
+				output += "//# sourceMappingURL=" + dest.split("/").pop() + ".map";
+				fs.writeFile(dest + ".map", gen_options.source_map.toString(), "utf-8");
+			}
+
+			return output;
 		}
 	}
 	return uglify;
@@ -31,17 +47,18 @@ function factory(uglify){
 
 if(global.define){
 	//loaded by dojo AMD loader
-	define(["dojo/has!host-node?dojo/node!uglify-js:"], factory);
+	define(["dojo/has!host-node?dojo/node!uglify-js:", "../../fs"], factory);
 }else{
 	//loaded in a node sub process
 	try{
 		var uglify = require("uglify-js");
+		var fs = require("fs");
 	}catch(e){}
-	uglify = factory(uglify);
+	uglify = factory(uglify, fs);
 	process.on("message", function(data){
 		var result = "", error = "";
 		try{
-			var result = uglify(data.text, data.options);
+			var result = uglify(data.text, data.options, data.dest, data.useSourceMaps);
 		}catch(e){
 			error = e.toString() + " " + e.stack;
 		}
