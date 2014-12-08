ee8416c485d8dbed57298187f9b0a81487550e5c
Colin Snover <github.com@zetafleet.com>
2014-12-08 08:49:18 -0800

    Updating metadata for 1.7.8

--- build/version.js
+++ build/version.js
@@ -1,8 +1,8 @@
 define([], function(){
 	var
-		rev = "$Rev$".match(/[0-9a-f]{7,}/),
+		rev = "$Rev: 7ad6178 $".match(/[0-9a-f]{7,}/),
 		version= {
-			major: 1, minor: 7, patch: 8, flag: "-pre",
+			major: 1, minor: 7, patch: 8, flag: "",
 			revision: rev ? rev[0] : NaN,
 			toString: function(){
 				var v= version;
