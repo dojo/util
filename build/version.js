11a14db05ec22680c891f188863f735755e79bfa
Colin Snover <github.com@zetafleet.com>
2015-01-19 08:02:13 -0800

    Updating source version to 1.8.11-pre

--- build/version.js
+++ build/version.js
@@ -1,8 +1,8 @@
 define([], function(){
 	var
-		rev = "$Rev: 8aea4bd $".match(/[0-9a-f]{7,}/),
+		rev = "$Rev$".match(/[0-9a-f]{7,}/),
 		version= {
-			major: 1, minor: 8, patch: 10, flag: "",
+			major: 1, minor: 8, patch: 11, flag: "-pre",
 			revision: rev ? rev[0] : NaN,
 			toString: function(){
 				var v= version;
