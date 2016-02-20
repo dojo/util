4a46a5332811be37e734381973cb4c482c3a921f
Dylan Schiemann <dylan@stinkbug.dojotoolkit.org>
2016-02-20 04:51:13 -0800

    Updating source version to 1.11.0-pre

--- build/version.js
+++ build/version.js
@@ -1,8 +1,8 @@
 define([], function(){
 	var
-		rev = "$Rev: a822d5d $".match(/[0-9a-f]{7,}/),
+		rev = "$Rev$".match(/[0-9a-f]{7,}/),
 		version= {
-			major: 1, minor: 11, patch: 0, flag: "-rc1",
+			major: 1, minor: 11, patch: 0, flag: "-pre",
 			revision: rev ? rev[0] : NaN,
 			toString: function(){
 				var v= version;
