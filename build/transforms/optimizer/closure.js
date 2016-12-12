bd14cf587be958c3d38aad91bdde9a4f43f900f4
Michael Van Sickle <mvansickle@sitepen.com>
2016-12-12 09:48:31 -0500

    add missing handlers for build options when using closure compiler on Java-driven builds

--- build/transforms/optimizer/closure.js
+++ build/transforms/optimizer/closure.js
@@ -61,13 +61,113 @@ define([
 			// force this option to false to prevent overly agressive code elimination (#18919)
 			options.setDeadAssignmentElimination(false);
 
-			for(var k in bc.optimizeOptions){
-				// some options need to pass as funtion argument
-				if (k === 'languageIn') {
-					options.setLanguageIn(jscomp.CompilerOptions.LanguageMode[bc.optimizeOptions[k]]);
+			var optimizeOptions = bc.optimizeOptions;
+
+			for(var k in optimizeOptions){
+				// Skip compilation level option
+				if (k === 'compilationLevel') {
 					continue;
 				}
-				options[k] = bc.optimizeOptions[k];
+
+				// Set functions requiring Java Enum values
+				if (k === 'languageIn') {
+					options.setLanguageIn(jscomp.CompilerOptions.LanguageMode.fromString(optimizeOptions[k].toUpperCase()));
+					continue;
+				}
+
+				if (k === 'languageOut') {
+					options.setLanguageOut(jscomp.CompilerOptions.LanguageMode.fromString(optimizeOptions[k].toUpperCase()));
+					continue;
+				}
+
+				if (k === 'variableRenaming') {
+					switch (optimizeOptions[k].toUpperCase()) {
+						case 'OFF': options.setVariableRenaming(jscomp.VariableRenamingPolicy.OFF);
+							break;
+						case 'LOCAL': options.setVariableRenaming(jscomp.VariableRenamingPolicy.LOCAL);
+							break;
+						case 'ALL': options.setVariableRenaming(jscomp.VariableRenamingPolicy.ALL);
+							break;
+					}
+					continue;
+				}
+
+				if (k === 'propertyRenaming') {
+					switch (optimizeOptions[k].toUpperCase()) {
+						case 'OFF': options.setPropertyRenaming(jscomp.PropertyRenamingPolicy.OFF);
+							break;
+						case 'ALL_UNQUOTED': options.setPropertyRenaming(jscomp.PropertyRenamingPolicy.ALL_UNQUOTED);
+							break;
+					}
+					continue;
+				}
+
+				if (k === 'checkGlobalThisLevel') {
+					switch (optimizeOptions[k].toUpperCase()) {
+						case 'ERROR': options.setCheckGlobalThisLevel(CheckLevel.ERROR);
+							break;
+						case 'WARNING': options.setCheckGlobalThisLevel(CheckLevel.WARNING);
+							break;
+						case 'OFF': options.setCheckGlobalThisLevel(CheckLevel.OFF);
+							break;
+					}
+					continue;
+				}
+
+				if (k in {
+					'removeUnusedVariables': 1,
+					'inlineVariables': 1,
+					'inlineFunctions': 1
+				}) {
+					set = "set" + k.charAt(0).toUpperCase() + k.substr(1);
+					switch (optimizeOptions[k].toUpperCase()) {
+						case 'ALL': options[set](jscomp.CompilerOptions.Reach.ALL);
+							break;
+						case 'LOCAL_ONLY': options[set](jscomp.CompilerOptions.Reach.LOCAL_ONLY);
+							break;
+						case 'NONE': options[set](jscomp.CompilerOptions.Reach.NONE);
+							break;
+					}
+					continue;
+				}
+
+				// Set functions with boolean values
+				if (k === 'dependencySorting') {
+					options.dependencyOptions.setDependencySorting(optimizeOptions[k]);
+					continue;
+				}
+
+				if (k in {
+					'assumeClosuresOnlyCaptureReferences': 1,
+					'closurePass': 1,
+					'coalesceVariableNames': 1,
+					'collapseAnonymousFunctions': 1,
+					'collapseProperties': 1,
+					'collapseVariableDeclarations': 1,
+					'computeFunctionSideEffects': 1,
+					'crossModuleCodeMotion': 1,
+					'crossModuleMethodMotion': 1,
+					'deadAssignmentElimination': 1,
+					'devirtualizePrototypeMethods': 1,
+					'extractPrototypeMemberDeclarations': 1,
+					'flowSensitiveInlineVariables': 1,
+					'foldConstants': 1,
+					'inlineConstantVars': 1,
+					'optimizeArgumentsArray': 1,
+					'removeDeadCode': 1,
+					'removeUnusedClassProperties': 1,
+					'removeUnusedPrototypeProperties': 1,
+					'removeUnusedPrototypePropertiesInExterns': 1,
+					'rewriteFunctionExpressions': 1,
+					'smartNameRemoval': 1
+				}) {
+					set = 'set' + k.charAt(0).toUpperCase() + k.substr(1);
+					options[set](optimizeOptions[k]);
+					continue;
+				}
+
+				// Otherwise assign value to key
+				options[k] = optimizeOptions[k];
 			}
 			// Must have non-null path to trigger source map generation, also fix version
 			options.setSourceMapOutputPath("");
