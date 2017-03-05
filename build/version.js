e20074175b74f2ba62896819ac54547414cbaf49
Dylan Schiemann <dylan@dojotoolkit.org>
2017-03-05 08:04:04 -0800

    Updating source version to 1.7.13-pre

--- build/version.js
+++ build/version.js
@@ -1,8 +1,8 @@
 define([], function(){
 	var
-		rev = "$Rev: 7d44850 $".match(/[0-9a-f]{7,}/),
+		rev = "$Rev$".match(/[0-9a-f]{7,}/),
 		version= {
-			major: 1, minor: 7, patch: 12, flag: "",
+			major: 1, minor: 7, patch: 13, flag: "-pre",
 			revision: rev ? rev[0] : NaN,
 			toString: function(){
 				var v= version;
