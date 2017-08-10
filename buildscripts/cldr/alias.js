3c3c4405537d5ef897a9f51434e1bc8aa682283b
Neil Roberts <neil@onebahamas.com>
2017-08-10 16:47:59 -0700

    refs #19043, Fix for field aliases that appear outside of calendars in CLDR root.xml
    (cherry picked from commit a100a185d0321ac9ed78379f42fc76483ba91b33)

--- buildscripts/cldr/alias.js
+++ buildscripts/cldr/alias.js
@@ -94,6 +94,10 @@ function _calculateAliasPath(bundle, name/*String*/){
 				mapping[LOCALE_ALIAS_SOURCE_PROPERTY] = src;
 				mapping[LOCALE_ALIAS_TARGET_PROPERTY] = bundle[localeAliasSource + LOCALE_ALIAS_MARK + i][LOCALE_ALIAS_TARGET_PROPERTY];
 				mapping[LOCALE_ALIAS_TARGET_BUNDLE] = bundle[localeAliasSource + LOCALE_ALIAS_MARK + i][LOCALE_ALIAS_TARGET_BUNDLE];
+				if (!mapping[LOCALE_ALIAS_TARGET_BUNDLE]) {
+					// TODO: Fix calendar.xsl to handle the new aliases in root.xml
+					continue;
+				}
 				//whether aliased to the bundle itself
 				mapping.inSelf = mapping[LOCALE_ALIAS_TARGET_BUNDLE] === name;
 				path.push(mapping);
