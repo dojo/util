daf6acd0b99a956875f09c51a3eb3fd1b6ea4ca0
Dylan Schiemann <dylan@dojotoolkit.org>
2019-11-29 16:09:18 +0000

    Updating metadata for 1.16.0

--- build/version.js
+++ build/version.js
@@ -1,8 +1,8 @@
 define([], function(){
 	var
-		rev = "$Rev$".match(/[0-9a-f]{7,}/),
+		rev = "$Rev: 90f5ee8 $".match(/[0-9a-f]{7,}/),
 		version= {
-			major: 1, minor: 16, patch: 0, flag: "-pre",
+			major: 1, minor: 16, patch: 0, flag: "",
 			revision: rev ? rev[0] : NaN,
 			toString: function(){
 				var v= version;
