08e58bf280dd7623cc8bdcd7ed32c4c8c1025df9
Dylan Schiemann <dylan@dojotoolkit.org>
2020-02-12 15:21:10 +0000

    Updating source version to 1.11.10-pre

--- build/version.js
+++ build/version.js
@@ -1,8 +1,8 @@
 define([], function(){
 	var
-		rev = "$Rev: 3ce7126 $".match(/[0-9a-f]{7,}/),
+		rev = "$Rev$".match(/[0-9a-f]{7,}/),
 		version= {
-			major: 1, minor: 11, patch: 9, flag: "",
+			major: 1, minor: 11, patch: 10, flag: "-pre",
 			revision: rev ? rev[0] : NaN,
 			toString: function(){
 				var v= version;
